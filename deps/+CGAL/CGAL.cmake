d2fe698dfa2ed74af162c8cb044a81d56ed92c1a
pyshrekek <rukereero@gmail.com>
2026-04-29 19:41:41 -0500

    fix patch

--- deps/+CGAL/CGAL.cmake
+++ deps/+CGAL/CGAL.cmake
@@ -2,7 +2,7 @@ add_cmake_project(
     CGAL
     URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.6.2.zip
     URL_HASH SHA256=29acaeee5a76a95029fac23131bb1c3a4a75df9a0e7e43b465a1f32d0628f45d
-    PATCH_COMMAND patch -p2 -i ${CMAKE_CURRENT_LIST_DIR}/CGAL.patch
+    PATCH_COMMAND sed -i.bak "s/this->base() == nullptr/this->g == nullptr/g" BGL/include/CGAL/boost/graph/iterator.h
 )
 
 include(GNUInstallDirs)
